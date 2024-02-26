08c19006a6e6fca433f9fab00e1ddcd46e57cb4c
Pavla Beránková <berankovap@protonmail.com>
2024-02-26 13:39:52 +0100

    [UI] Add RootView with TabView (home, movies, celebrities, info)

--- Div/DivApp.swift
+++ Div/DivApp.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct DivApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                RootView()
+            }
         }
     }
 }
---
+++ Div/RootView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct RootView: View {
+
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            TabView {
+                homeTab
+                moviesTab
+                celebritiesTab
+                infoTab
+            }
+        }
+    }
+}
+
+// MARK: - EXTENSION
+extension RootView {
+    private var homeTab: some View {
+        Text("Úvodní obrazovka")
+            .tabItem {
+                VStack {
+                    Image(systemName: "square.grid.2x2.fill")
+                    Text("Domů")
+                }
+            }
+    }
+
+    private var moviesTab: some View {
+        Text("Filmy")
+            .tabItem {
+                VStack {
+                    Image(systemName: "movieclapper")
+                    Text("Filmy")
+                }
+            }
+    }
+
+    private var celebritiesTab: some View {
+        Text("Osobnosti")
+            .tabItem {
+                VStack {
+                    Image(systemName: "person.crop.square")
+                    Text("Osobnosti")
+                }
+            }
+    }
+
+    private var infoTab: some View {
+        Text("Informace & kontakty")
+            .tabItem {
+                VStack {
+                    Image(systemName: "info.square")
+                    Text("Info")
+                }
+            }
+    }
+}
+
+// MARK: - PREVIEW
+#Preview {
+    RootView()
+}
